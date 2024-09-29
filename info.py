e65c9416db6c3a7c8204e20620470760f40673a3
sri <sridurga.bolli@unh.edu>
2024-09-29 17:41:56 -0400

    SCRUM-23 Integrated the chat.html with app.py and info.py

--- info.py
+++ info.py
@@ -1,15 +1,8 @@
 Internshipinfo = {
-    "How is the final grade composed?": "The final grade consists of three components: Class Attendance (10%), Sprint Grade (60%), and Final Project Report (20%).",
-    "What percentage of the final grade is based on class attendance?": "10% of the final grade is based on class attendance of all required meetings.",
-    "How is the Sprint Grade calculated?": "The Sprint Grade is calculated as: Teamwork Grade * Sprint Grade.",
-    "What is the Teamwork Grade based on?": "The Teamwork Grade is based on peer evaluation for each of the three sprints.",
-    "How many sprints are there in the project?": "There are three sprints mentioned in the project.",
-    "What is the Sprint Grade based on?": "The Sprint Grade is based on the technical aspect of the product and team project management.",
-    "What percentage of the final grade is the Final Project Report worth?": "The Final Project Report is worth 20% of the final grade.",
-    "What is the credit hour workload estimate for the project?": "The credit hour workload estimate is a minimum of 45 hours of student academic work per credit per term.",
-    "Is there any additional homework mentioned?": "Yes, there is additional homework in project management and development tools, worth 10% of the final grade." ,
     "what is the instructor name ?": "The instructor name is Karen Jin.",
     "Can I have a meeting with professor ?" :"Yes Based on her avaliablity and working days .",
     "The required credits for entire internship course is ?": "3.",
-    "I have done two credits how many credits required still for the internship course? " :"1"
+    "I have done two credits how many credits required still for the internship course? " :"1",
+    "Can I meet professor on weekends? "  :"No.",
+    "On What days do we have sprint meetings?" :" Mon,Wed, Fri"
 }
